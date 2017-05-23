54137767ff8b2eac399d02718e7b8f26fedcc773
Dm <630372254@qq.com>
2017-05-23 17:28:19 +0800

    start learn

---
+++ LeetCode/src/com/play/easy/HammingDistance.java
@@ -0,0 +1,23 @@
+package com.play.easy;
+
+/**
+ * 
+ * Input: x = 1, y = 4
+ * Output: 2
+ * Explanation:
+ * 1   (0 0 0 1)
+ * 4   (0 1 0 0)
+ *        ↑   ↑
+ * The above arrows point to positions where the corresponding bits are different.
+ * @author DM
+ *
+ */
+
+public class HammingDistance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
